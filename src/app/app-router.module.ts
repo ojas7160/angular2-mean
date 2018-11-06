576a4ab41cf4be57ef478deea7c4a8ab6510c1a8
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2018-11-06 11:16:27 +0530

    routing done

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponentComponent} from './posts-component/posts-component.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
 
 
 // Routes are simple javascript object where we define to which url for which part of our app should be presented
 const routes: Routes = [
-{path: '', component: PostsComponentComponent}
+	{path: '', component: PostsComponentComponent},
+	{path: 'posts', component: AllPostsComponent}
 ];
 @NgModule({
-imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRouterModule{
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostsComponentComponent } from './posts-component/posts-component.component';
 import { AppRouterModule } from './app-router.module';
+import { AllPostsComponent } from './all-posts/all-posts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponentComponent
+    PostsComponentComponent,
+    AllPostsComponent
   ],
   imports: [
     BrowserModule,
