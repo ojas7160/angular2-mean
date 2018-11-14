895a1e39dcf50739ba5067c40c8636b171a764c8
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2018-11-14 18:11:09 +0530

    signup form

--- src/app/angularMaterial.module.ts
+++ src/app/angularMaterial.module.ts
@@ -3,16 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatPaginatorModule, MatProgressSpinnerModule, MatDialogModule } from '@angular/material';
 
 @NgModule({
-  imports: [
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatPaginatorModule,
-    MatProgressSpinnerModule,
-    MatDialogModule
-  ],
   exports: [
     MatInputModule,
     MatCardModule,
