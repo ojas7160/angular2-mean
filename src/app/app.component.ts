fef4dcec1cb3fc5cd8eed253aaddc30086ce8313
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2019-02-25 17:56:46 +0530

    event binding input output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-angular2-app';
+  @Input()  allBlogs: any = [];
+  
+  onBlogAdded(blog) {
+    this.allBlogs.push(blog);
+  }
 }
--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-blogs',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogs.component.css']
 })
 export class BlogsComponent implements OnInit {
-
+  @Output() blogAdded = new EventEmitter();
+  blogs: any = [];
+  title: any;
+  description: any;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addBlog() {
+    // this.blogs.push({title: this.title, description: this.description});
+    this.blogAdded.emit({title: this.title, description: this.description});
   }
 
 }
